8697d2cdf10d6610b7356efe88064b4c049232de
Heet Chanchad <chanchah@sheridancollege.ca>
2026-01-28 16:20:44 -0500

    Added a method that says hello when called!

---
+++ Cloned_ICE4/src/main/java/com/mycompany/cloned_ice4/Cloned_ICE4.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cloned_ice4;
+
+/**
+ *
+ * @author heetc
+ */
+public class Cloned_ICE4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    static void sayHello() {
+    System.out.println("Just wanted to say Hello!");
+}
+
+}
+
